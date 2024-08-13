8d705285aba409a9c8d9ab119911a364c71a58cd
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2024-08-13 16:46:25 +0300

    additional test cases added

---
+++ tests/cases/25-image-by-url-4o-12t-low.ts
@@ -0,0 +1,23 @@
+import OpenAI from "openai";
+
+export default <OpenAI.Chat.ChatCompletionCreateParamsNonStreaming>{
+  model: "gpt-4o",
+  messages: [
+    {
+      role: "user",
+      content: [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://raw.githubusercontent.com/n0isy/openai-tokens-count/images-input/tests/__fixtures__/12t-2048x1025.png",
+            "detail": "low"
+          }          
+        },
+      ],
+    }
+  ],
+};
