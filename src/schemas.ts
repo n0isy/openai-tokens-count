e9736f8434dc1a4a5e3e5e8c27f2539c1b3402f5
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2024-08-13 16:47:47 +0300

    messageSchema extended

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,51 +1,91 @@
 export const schemas = {
-  "title": "Tool Schema",
-  "type": "array",
-  "items": {
-  "type": "object",
-    "properties": {
-    "function": {
-      "type": "object",
-        "properties": {
-        "name": { "type": "string" },
-        "description": { "type": "string" },
-        "parameters": { "type": "object" }
+  title: "Tool Schema",
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      function: {
+        type: "object",
+        properties: {
+          name: { type: "string" },
+          description: { type: "string" },
+          parameters: { type: "object" },
+        },
+        required: ["name"],
       },
-      "required": ["name"]
+      type: { type: "string", enum: ["function"] },
     },
-    "type": { "type": "string", "enum": ["function"] }
+    required: ["function", "type"],
   },
-  "required": ["function", "type"]
-}
-}
+};
 
 export const messageSchema = {
-  "title": "Message Schema",
-    "type": "object",
-    "properties": {
-    "role": { "type": "string" },
-    "content": { "type": ["string", "array", "null"] },
-    "refusal": { "type": ["string", "null"] },
-    "name": { "type": "string" },
-    "tool_calls": {
-      "type": "array",
-        "items": {
-        "type": "object",
-          "properties": {
-          "id": { "type": "string" },
-          "function": {
-            "type": "object",
-              "properties": {
-              "name": { "type": "string" },
-              "arguments": { "type": "string" }
-            },
-            "required": ["name", "arguments"]
-          },
-          "type": { "type": "string", "enum": ["function"] }
+  title: "Message Schema",
+  type: "object",
+  properties: {
+    role: { type: "string" },
+    content: {
+      oneOf: [
+        {
+          "type": "string",
         },
-        "required": ["id", "function", "type"]
-      }
-    }
+        {
+          type: "array",
+          items: {
+            oneOf: [
+              {
+                "type": "string",
+              },
+              {
+                type: "object",
+                properties: {
+                  type: { type: "string", enum: ["text"] },
+                  text: { type: "string" },
+                },
+                required: ["type", "text"],
+              },
+              {
+                type: "object",
+                properties: {
+                  type: { type: "string", enum: ["image_url"] },
+                  image_url: {
+                    type: "object",
+                    properties: {
+                      url: { type: "string" },
+                      detail: { type: "string" },
+                    },
+                    required: ["url", "detail"],
+                  },
+                },
+                required: ["type", "image_url"],
+              },
+            ],
+          },
+        },
+        
+      ]
+    },
+    refusal: { type: ["string", "null"] },
+    name: { type: "string" },
+    tool_calls: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          id: { type: "string" },
+          function: {
+            type: "object",
+            properties: {
+              name: { type: "string" },
+              arguments: { type: "string" },
+            },
+            required: ["name", "arguments"],
+          },
+          type: { type: "string", enum: ["function"] },
+        },
+        required: ["id", "function", "type"],
+      },
+    },
   },
-  "required": ["role"]
-}
+  required: ["role"],
+};
